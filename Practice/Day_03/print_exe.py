0990c9b07373254dd19329270e71b3913e8230ec
kamallearner123 <kamalbec2004@gmail.com>
2026-09-21 15:19:31 +0530

    Updating course

---
+++ Practice/Day_03/print_exe.py
@@ -0,0 +1,7 @@
+# Program 1.1: Verifying Python Version and Environment Setup
+import sys
+import platform
+
+print(f"Python Version: {platform.python_version()}")
+print(f"Executable Path: {sys.executable}")
+print(f"Platform: {platform.platform()}")
